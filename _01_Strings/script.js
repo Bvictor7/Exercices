139dc963449014b83be50434d07c240427c1a71f
Bvictor7 <162301500+Bvictor7@users.noreply.github.com>
2024-11-06 13:38:17 +0100

    Add files via upload

---
+++ _01_Strings/script.js
@@ -0,0 +1,33 @@
+/*excercice 0*/
+console.log("JavaScript est super");
+
+/*exercice 1*/
+console.log("JavaScript c'est facile.");
+
+/*exercice 2*/
+console.log("C'est Lao Tseu qui a dit:\"L'échec est le fondement de la réussite\".");
+
+/*exercice 3 */
+console.log("Afficher un \\ n'est pas si évident.");
+
+/*exercice 4*/
+console.log ("Ceci est la première ligne \nCeci est la deuxième ligne" );
+
+/*exercice 5*/
+console.log("Un jour le JavaScript n'aura plus de secrets pour moi\nLire du code ne sera plus une gageure mais une joie\nMes doigts seront habitués à tous ces symboles particuliers\nEt ma créativité de développeur pourra enfin s'exprimer"
+);
+
+console.log(`Un jour le JavaScript n'aura plus de secrets pour moi
+Lire du code ne sera plus une gageure mais une joie
+Mes doigts seront habitués à tous ces symboles particuliers
+Et ma créativité de développeur pourra enfin s'exprimer`
+);
+
+/*chassez le bogue!0*/
+
+let prenomClient = ("Bob")
+console.log( "Bonjour "+ prenomClient +" Aujourd'hui nous faisons une promo \"la fidélité paye\"!.")
+
+/*Tom*/
+let message = ("Bonjour "+ prenomClient +" Aujourd'hui nous faisons une promo \"la fidélité paye\"!.")
+console.log(message);
